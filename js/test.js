705f45ec5d6f580e366132748ececbf179af8790
Garry <garry3331@icloud.com>
2023-01-05 19:00:25 +0300

    first commit for newproject

---
+++ js/test.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+function function_name(n) {
+	if (n == 1) return 1;
+	return `${function_name(n -1)} ${n}`;
+}
+
+
+
+
+
+
+console.log(function_name(10));
+//alert(function_name(4));
+
+
+
+
+
